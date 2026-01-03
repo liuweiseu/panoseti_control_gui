8684b449e78eb9660f53d27bd6214828b0077d95
Wei Liu <liuwei_berkeley@berkeley.edu>
2026-01-03 15:45:50 -0800

    optimized the functions for updating plots.

--- grpc_thread.py
+++ grpc_thread.py
@@ -1,18 +1,28 @@
+import sys
 import asyncio
 import threading
-import time
+import json
+import logging
+import signal
 
 from PyQt6.QtCore import QObject, pyqtSignal
+
+sys.path.insert(0, 'panoseti_grpc')
+from daq_data.client import AioDaqDataClient
+import daq_data.cli as cli
 
 class DataSignal(QObject):
     new_data = pyqtSignal(object)
 
 class AsyncioThread(threading.Thread):
-    def __init__(self):
+    def __init__(self, daq_config_path, net_config_path):
         super().__init__(daemon=True)
         self.loop = asyncio.new_event_loop()
         self.tasks = set()
         self.data_signal = DataSignal()
+        self.daq_config_path = daq_config_path
+        self.net_config_path = net_config_path
+        self.shutdown_event = None
 
     def run(self):
         asyncio.set_event_loop(self.loop)
@@ -21,11 +31,40 @@ class AsyncioThread(threading.Thread):
     def stop(self):
         self.loop.call_soon_threadsafe(self.loop.stop)
 
-    async def send_data(self):
+    async def send_data_demo(self):
         while True:
             self.data_signal.new_data.emit('hello world')
             await asyncio.sleep(1)
-        
+    
+    async def fetch_data(self):
+        print('start fetch data.')
+        print(self.daq_config_path)
+        print(self.net_config_path)
+        # use self.addc
+        self.shutdown_event = asyncio.Event()
+        async with AioDaqDataClient(self.daq_config_path, self.net_config_path, stop_event=self.shutdown_event, log_level=logging.DEBUG) as addc:
+            print('created')
+            host = None
+            hp_io_cfg_path = 'panoseti_grpc/daq_data/config/hp_io_config_simulate.json'
+            with open(hp_io_cfg_path, "r") as f:
+                hp_io_cfg = json.load(f)
+            await addc.init_hp_io(host, hp_io_cfg, timeout=15.0)
+            valid_daq_hosts = await addc.get_valid_daq_hosts()
+            print('valid_daq_hosts:', valid_daq_hosts)
+            if host is not None and host not in valid_daq_hosts:
+                raise ValueError(f"Invalid host: {host}. Valid hosts: {valid_daq_hosts}")
+            stream_images_responses = await addc.stream_images(
+            host,
+            stream_movie_data=False,
+            stream_pulse_height_data=True,
+            update_interval_seconds=1.0,
+            module_ids=[],
+            parse_pano_images=True,
+            wait_for_ready=True,
+            )
+            async for parsed_pano_image in stream_images_responses:
+                self.data_signal.new_data.emit(parsed_pano_image)
+
     def submit(self, coro):
         def _create():
             task = asyncio.create_task(coro)
--- mainwin_op.py
+++ mainwin_op.py
@@ -6,20 +6,17 @@ import logging, json
 from pathlib import Path
 import pyqtgraph as pg
 import numpy as np
-import time
 
 from mainwin import Ui_MainWindow
 from data_config_op import DataConfigWin, DataConfigOp
-import sys, asyncio, signal
-sys.path.insert(0, 'panoseti_grpc')
-from daq_data.client import AioDaqDataClient
-import daq_data.cli as cli
+import asyncio
 
 # from qasync import asyncSlot
 from grpc_thread import AsyncioThread
 
 from utils import create_logger
 
+NUM_PLOTS = 4
 class MainWinOp(QMainWindow, Ui_MainWindow):
     def __init__(self, root_dir_config='root_dir.json'):
         create_logger('mainwin.log', 'MAINWIN', 'a')
@@ -47,25 +44,19 @@ class MainWinOp(QMainWindow, Ui_MainWindow):
             self.append_log(f'\"{root_dir_config}\" doesn\'t exist!')
             self.append_log('********************************************')
         # add static figure by default
-        pixmap = QPixmap("placeholder.png")
-        pixmap = pixmap.scaled(350, 350) 
-        self.static_label = []
-        for i in range(4):
-            label = QLabel()
-            label.setPixmap(pixmap)
-            label.setScaledContents(True)
-            self.static_label.append(label)
+        self.static_label = [None] * NUM_PLOTS
         for r in range(2):
             for c in range(2):
-                self.view_layout.addWidget(self.static_label[r*2+c], r,c,1,1)
-        self.plot_widgets = []
-        self.timers = []
-        self.imgs = []
+                self.set_placeholder(r, c)
+        self.plot_widgets = [None] * NUM_PLOTS
+        self.timers = [None] * NUM_PLOTS
+        self.imgs = [None] * NUM_PLOTS
         self.shutdown_event = None
-        self.grpc_thread = AsyncioThread()
+        self.grpc_thread = AsyncioThread(self.ps_grpc_daq, self.ps_grpc_net)
         self.grpc_thread.start()
         self.setup_signal_functions()
     
+
     # ------------------------------------------------------------------------
     # Sub Window creation
     # ------------------------------------------------------------------------
@@ -76,6 +67,18 @@ class MainWinOp(QMainWindow, Ui_MainWindow):
         self.data_config_op.setup_signal_functions()
         self.data_config_win.show()
 
+    # ------------------------------------------------------------------------
+    # Set placeholder
+    # ------------------------------------------------------------------------
+    def set_placeholder(self, r, c):
+        i = r * 2 + c
+        pixmap = QPixmap("placeholder.png")
+        pixmap = pixmap.scaled(350, 350) 
+        label = QLabel()
+        label.setPixmap(pixmap)
+        label.setScaledContents(True)
+        self.static_label[i] = label
+        self.view_layout.addWidget(self.static_label[i], r,c,1,1)
     # ------------------------------------------------------------------------
     # Low level APIs
     # ------------------------------------------------------------------------
@@ -96,107 +99,38 @@ class MainWinOp(QMainWindow, Ui_MainWindow):
     def start_grpc_clicked(self):
         asyncio.run_coroutine_threadsafe(self.show_plot(), self.loop)
 
-    async def aio_show_plot(self):
-        self.append_log(f'Start panoseti grpc...')
-        # 移除占位图片
-        if self.static_label[0] is not None:
-            for i in range(4):
-                self.view_layout.removeWidget(self.static_label[i])
-                self.static_label[i].deleteLater()
-                self.static_label[i] = None
-        # 创建 pyqtgraph 动态图
-        if len(self.plot_widgets) < 4 :
-            for r in range(2):
-                for c in range(2):
-                    plot_widget = pg.PlotWidget()
-                    self.plot_widgets.append(plot_widget)
-                    self.view_layout.addWidget(plot_widget, r, c, 1, 1)
-                    # 创建 32x32 示例数据
-                    data = np.random.rand(32, 32)
-                    h, w = data.shape
-                    # 在 pyqtgraph 中显示 2D 图
-                    img = pg.ImageItem(data)
-                    self.imgs.append(img)
-                    plot_widget.addItem(img)
-                    img.setRect(0,0,w,h)
-                    # 可选：去掉坐标轴
-                    plot_widget.hideAxis('bottom')
-                    plot_widget.hideAxis('left')
-                    # 设置color map
-                    cmap = pg.colormap.get('plasma')  # PyQtGraph >=0.13
-                    img.setLookupTable(cmap.getLookupTable(0.0, 1.0, 256))
-                    # set title
-                    plot_widget.setTitle("Winter", color='w', size='12pt')
-                    # 定时更新曲线
-        # use self.addc
-        async with self.addc as addc:
-            host = None
-            hp_io_cfg_path = 'panoseti_grpc/daq_data/config/hp_io_config_simulate.json'
-            with open(hp_io_cfg_path, "r") as f:
-                hp_io_cfg = json.load(f)
-            await addc.init_hp_io(host, hp_io_cfg, timeout=15.0)
-            valid_daq_hosts = await addc.get_valid_daq_hosts()
-            print('valid_daq_hosts:', valid_daq_hosts)
-            if host is not None and host not in valid_daq_hosts:
-                raise ValueError(f"Invalid host: {host}. Valid hosts: {valid_daq_hosts}")
-            
-            stream_images_responses = await addc.stream_images(
-            host,
-            stream_movie_data=True,
-            stream_pulse_height_data=True,
-            update_interval_seconds=1.0,
-            module_ids=[],
-            parse_pano_images=True,
-            wait_for_ready=True,
-            )
-            self.append_log(f'go!')
-            async for parsed_pano_image in stream_images_responses:
-                self.imgs[0].setImage(parsed_pano_image['image_array'])
-            self.append_log(f'Done!')
-        
-        for i in range(4):
-            timer = pg.QtCore.QTimer()
-            self.timers.append(timer)
-            timer.timeout.connect(lambda i=i: self.imgs[i].setImage(np.random.rand(32,32)))
-            timer.start(100)
+    def create_plot(self, r, c):
+        i = r * 2 + c
+        if self.static_label[i] is not None:
+            self.view_layout.removeWidget(self.static_label[i])
+            self.static_label[i].deleteLater()
+            self.static_label[i] = None
 
-    def show_plot(self):
-        self.append_log(f'Start panoseti grpc...')
-        # 移除占位图片
-        if self.static_label[0] is not None:
-            for i in range(4):
-                self.view_layout.removeWidget(self.static_label[i])
-                self.static_label[i].deleteLater()
-                self.static_label[i] = None
-        # 创建 pyqtgraph 动态图
-        if len(self.plot_widgets) <4 :
-            for r in range(2):
-                for c in range(2):
-                    plot_widget = pg.PlotWidget()
-                    self.plot_widgets.append(plot_widget)
-                    self.view_layout.addWidget(plot_widget, r, c, 1, 1)
-                    # 创建 32x32 示例数据
-                    data = np.random.rand(32, 32)
-                    h, w = data.shape
-                    # 在 pyqtgraph 中显示 2D 图
-                    img = pg.ImageItem(data)
-                    self.imgs.append(img)
-                    plot_widget.addItem(img)
-                    img.setRect(0,0,w,h)
-                    # 可选：去掉坐标轴
-                    plot_widget.hideAxis('bottom')
-                    plot_widget.hideAxis('left')
-                    # 设置color map
-                    cmap = pg.colormap.get('plasma')  # PyQtGraph >=0.13
-                    img.setLookupTable(cmap.getLookupTable(0.0, 1.0, 256))
-                    # set title
-                    plot_widget.setTitle("Winter", color='w', size='12pt')
-                    # 定时更新曲线
-        for i in range(4):
-            timer = pg.QtCore.QTimer()
-            self.timers.append(timer)
-            timer.timeout.connect(lambda i=i: self.imgs[i].setImage(np.random.rand(32,32)))
-            timer.start(100)
+            plot_widget = pg.PlotWidget()
+            self.plot_widgets[i] = plot_widget
+            self.view_layout.addWidget(plot_widget, r, c, 1, 1)
+            # create random data for default viewer
+            data = np.random.rand(32, 32)
+            h, w = data.shape
+            # show 2D img
+            img = pg.ImageItem(data)
+            self.imgs[i] = img
+            plot_widget.addItem(img)
+            img.setRect(0,0,w,h)
+            # remove axis
+            plot_widget.hideAxis('bottom')
+            plot_widget.hideAxis('left')
+            # set color map
+            cmap = pg.colormap.get('plasma')  # PyQtGraph >=0.13
+            img.setLookupTable(cmap.getLookupTable(0.0, 1.0, 256))
+            # set title
+            plot_widget.setTitle("Winter", color='w', size='12pt')
+        else:
+            pass
+
+    def update_plot(self, r, c, data):
+        i = r * 2 + c
+        self.imgs[i].setImage(data)
     # ------------------------------------------------------------------------
     # Signal functions
     # ------------------------------------------------------------------------
@@ -225,14 +159,22 @@ class MainWinOp(QMainWindow, Ui_MainWindow):
 
     def submit_task(self):
         self.append_log('Start Task.')
-        self.grpc_thread.submit(self.grpc_thread.send_data())
+        self.grpc_thread.submit(self.grpc_thread.fetch_data())
 
     def cancel_all(self):
         self.append_log('Cancel Task.')
+        self.grpc_thread.loop.call_soon_threadsafe(self.grpc_thread.shutdown_event.set)
         self.grpc_thread.cancel_all()
+        self.set_placeholder(0, 0)
     
     def print_log(self, data):
-        self.append_log(data)
+        self.append_log(data['type'])
+        self.append_log(str(data['module_id']))
+        self.append_log(str(data['header']))
+        self.append_log(str(data['frame_number']))
+        self.append_log(str(data['file']))
+        self.create_plot(0, 0)
+        self.update_plot(0, 0, data['image_array'])
     # ------------------------------------------------------------------------
     # Setup signal function
     # ------------------------------------------------------------------------
